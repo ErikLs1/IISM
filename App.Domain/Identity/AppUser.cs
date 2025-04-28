ba44a5d5fb59232bf4e2914632a361715163318c
eriklihhats <eriklihhats@gmail.com>
2025-04-28 10:05:32 +0300

    create uow

--- App.Domain/Identity/AppUser.cs
+++ App.Domain/Identity/AppUser.cs
@@ -4,9 +4,6 @@ namespace App.Domain.Identity;
 
 public class AppUser : BaseUser<AppUserRole>
 {
-    /*
-    public Person? Person { get; set; }
-    */
     public ICollection<Person>? Persons { get; set; }
 
 }
--- WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc341499c7596bbaf78d7dc817fdaffe8881329d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317bf45d3f464aa3f3167320f02e14eaf300eb00")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
